c97076f421703498206555891ffcde7d5424d2a2
Rafiul29 <rafiulislam4472@gmail.com>
2026-01-30 01:16:11 +0600

    socail login for google provider

--- components/modules/authentication/social-google.tsx
+++ components/modules/authentication/social-google.tsx
@@ -1,9 +1,43 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
 
 const SocialGoogle = ({ title }: { title: string }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGoogleLogin = async () => {
+    setIsLoading(true);
+    try {
+      const data = await authClient.signIn.social({
+        provider: "google",
+        callbackURL: window.location.origin,
+      });
+    } catch (error: any) {
+      toast.error(error.message || "Failed to connect to Google");
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button variant="outline" type="button">
-      {title}
+    <Button
+      onClick={handleGoogleLogin}
+      variant="outline"
+      type="button"
+      disabled={isLoading}
+      className="w-full"
+    >
+      {isLoading ? (
+        <span className="flex items-center gap-2">
+          <Spinner />
+          <span>Connecting...</span>
+        </span>
+      ) : (
+        <>{title}</>
+      )}
     </Button>
   );
 };
